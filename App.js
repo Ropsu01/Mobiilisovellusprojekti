09723108b8a78599d98df5bf844f8023be25eb74
nooruuw <nooramaria.ylitalo@gmail.com>
2024-03-20 09:24:33 +0200

    lisäyksiä

--- App.js
+++ App.js
@@ -1,21 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import Weather from './screens/Weather';
 import NavBar from './components/NavBar';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View >
       <NavBar />
-      <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
--- components/NavBar.js
+++ components/NavBar.js
@@ -1,24 +1,25 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { View, Text } from 'react-native'
 import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
 import Home from '../screens/Home';
 import Tasks from '../screens/Tasks';
-import Weather from '../screens/Weather';
 import Calendar from '../screens/Calendar';
+import Weather from '../screens/Weather';
 import Jokes from '../screens/Jokes';
 import { NavigationContainer } from '@react-navigation/native';
+
 
 const Tab = createBottomTabNavigator();
 
 export default function NavBar() {
     return (
-        <NavigationContainer>
-            <Tab.Navigator>
+        <NavigationContainer >
+            <Tab.Navigator > 
                 <Tab.Screen name="Home" component={Home} />
-                <Tab.Screen name="Tasks" component={Tasks}/>
-                <Tab.Screen name="Calendar" component={Calendar}/> 
-                <Tab.Screen name="Weather" component={Weather}/>
-                <Tab.Screen name="Jokes" component={Jokes} /> 
+                <Tab.Screen name="Tasks" component={Tasks} />
+                <Tab.Screen name="Calendar" component={Calendar} />
+                <Tab.Screen name="Weather" component={Weather} />
+                <Tab.Screen name="Jokes" component={Jokes} />
             </Tab.Navigator>
         </NavigationContainer>
     );
