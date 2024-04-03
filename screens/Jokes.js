e2f14b8facecba9adeee01d1b96426960672a12b
tompih <112546914+tompih@users.noreply.github.com>
2024-04-03 12:05:15 +0300

    ,

--- screens/Jokes.js
+++ screens/Jokes.js
@@ -1,99 +1,103 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
-import { AppRegistry } from 'react-native';
-import App from '../App';
-AppRegistry.registerComponent('MyApp', () => App);
+import { View, Text, TouchableOpacity, StyleSheet, ScrollView } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
+import firebase from 'firebase/app';
+import 'firebase/database';
+
+// Alusta Firebase-app
+const firebaseConfig = {
+  // Lisää Firebase-projektisi konfiguraatio tähän
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+const db = firebase.database();
 
 const JokesAndFacts = () => {
   const [joke, setJoke] = useState('');
   const [funFact, setFunFact] = useState('');
-  const [lastUpdatedDay, setLastUpdatedDay] = useState(0);
+  const [favorites, setFavorites] = useState([]);
+  const [showFavorites, setShowFavorites] = useState(false);
 
-  const jokes = [
-    "Mitä astronautti tokaisi löydettyään luurangon kuusta? - Mitä kuuluu?",
-    "Mikä on Maija Mehiläisen isän nimi? - Faija Mehiläinen!",
-    "Miksi hai ei voi olla taksikuski? - Koska se voi olla liikenteelle haitaksi!",
-    "Kaksi lehmää oli kaurismetsällä. Hetken päästä toinen näki kauriin ja kuiskasi toiselle. - Shh... Ei ammuta",
-    "Mitä kala tekee tupakkatehtaassa? - Sätkii",
-    "Mikä on maailman paras vihannes? - Best Selleri",
-    "Mitä laivan työntekijä pukee päälleen mennessään töihin? - Alusvaatteet.",
-    "Kuka vetää lintujen AA-kerhoa? - Selvä pyy.",
-    "Mistä kankaasta kummitusten vaatteet on tehty? - Buuvillasta.",
-    "Mikä Postimies Patesta tuli, kun hän jäi eläkeelle? - Pate",
-    "Mikä on pilates suomeksi? - Melilosvot",
-    "Mitä Puolalainen teki altaassa? - Polski",
-    "Mikä on maailman yksinkertaisin peli? - Simppeli",
-    "Miksi Aki syö pullaa sateella? - Ei vaivannut keliakia",
-    "Mikä tuli USA:n jälkeen? - USB",
-    "Mitä Espanjalainen sanoi nähdessään hyvän diaesityksen? - Buenos Dias",
-    "Mitä eroa on mustilla ja vihreillä oliiveilla? - Musti on koira",
+  // Haetaan vitsit ja faktat Firebasesta
+  useEffect(() => {
+    const fetchData = () => {
+      const jokesRef = db.ref('jokes');
+      const factsRef = db.ref('facts');
 
-    // Add more jokes here
-  ];
+      jokesRef.once('value', snapshot => {
+        const jokesData = snapshot.val();
+        const randomJokeIndex = Math.floor(Math.random() * jokesData.length);
+        setJoke(jokesData[randomJokeIndex]);
+      });
 
-  const facts = [
-    "Suomi on maailman 'sisukkain' kansa, sillä sanalla ei ole käännöstä muille kielille.",
-    "Suomen lipun sininen väri edustaa tuhansia järvemme, ja valkoinen puhtautta ja lunta.",
-    "Aivastat noin 100 kertaa päivässä, jos olet allerginen.",
-    "Krokotiilit itse asiassa itkevät kun he syövät.",
-    "Karhut ovat oikeakätisiä.",
-    "Koira on ainoa eläin, joka osaa lukea ihmisen kasvoja.",
-    "Auringonpimennys tapahtuu noin joka 18 kuukauden välein ja kestää vain muutaman minuutin.",
-    "Maapallon kiertorata on noin 940 miljoonaa kilometriä.",
-    "Kuun painovoima on noin 1/6 maan painovoimasta.",
-    "Siilillä on noin 7000 piikkiä kehossaan.",
-    "Krokotiilit voivat elää jopa 100 vuotta.",
-    "Muurahaiset nukkuvat 8 minuuttia päivässä, mikä tekee niistä eläinmaailman lyhimmän unen nukkujat.",
-    "Eiffel-torni voi olla jopa 15 senttiä korkeampi kesällä lämpölaajenemisen vuoksi.",
-    "Skotlannin kansalliseläin on yksisarvinen",
-    "Mustekalalla on kolme sydäntä.",
-    "Sitruuna kelluu, mutta lime ei.",
-    "Eläimet voivat olla allergisia ihmisille.",
-    "Australia on leveämpi kuin Kuu.",
-    "Paavi ei voi toimia elinluovuttajana.",
-    "Lepakot ovat ainoita lentäviä nisäkkäitä",
-    "Ihmiskehon pienin luu sijaitsee korvassa.",
-    "Maailman vanhin kirjoitettu resepti on yli 4000 vuotta vanha.",
-    "Maailmassa on vain yksi maa, jossa ei ole hyttysiä - Islanti.",
-    "Suomessa on enemmän saunoja kuin autoja.",
-    "Kynnet kasvavat nopeammin kesällä kuin talvella.",
-    "Venäjällä on 11 eri aikavyöhykettä.",
+      factsRef.once('value', snapshot => {
+        const factsData = snapshot.val();
+        const randomFactIndex = Math.floor(Math.random() * factsData.length);
+        setFunFact(factsData[randomFactIndex]);
+      });
+    };
 
-    // Add more facts here
-  ];
+    fetchData();
+  }, []);
 
-  // Funktio valitsee satunnaisen vitsin tai faktan
-  const getRandomContent = (contentArray) => {
-    const randomIndex = Math.floor(Math.random() * contentArray.length);
-    return contentArray[randomIndex];
+  // Tallenna suosikkilista Firebaseen
+  const saveFavoritesToFirebase = () => {
+    db.ref('favorites').set(favorites);
   };
 
-  // Funktio päivittää vitsin ja faktan päivittäin
-  const updateDailyContent = () => {
-    const today = new Date().getDate();
-    if (today !== lastUpdatedDay) {
-      const newJoke = getRandomContent(jokes);
-      const newFact = getRandomContent(facts);
-      setJoke(newJoke);
-      setFunFact(newFact);
-      setLastUpdatedDay(today);
+  // Lataa suosikkilista Firebasesta
+  useEffect(() => {
+    const fetchFavorites = () => {
+      db.ref('favorites').once('value', snapshot => {
+        const favoritesData = snapshot.val() || [];
+        setFavorites(favoritesData);
+      });
+    };
+
+    fetchFavorites();
+  }, []);
+
+  // Lisää tai poista suosikkilistalta
+  const toggleFavorite = (content) => {
+    if (favorites.includes(content)) {
+      const updatedFavorites = favorites.filter((item) => item !== content);
+      setFavorites(updatedFavorites);
+    } else {
+      setFavorites([...favorites, content]);
     }
   };
 
-  // Kutsutaan päivitysfunktiota komponentin ensimmäisellä renderöinnillä
-  useEffect(() => {
-    updateDailyContent();
-  }, []);
-
   return (
     <View style={styles.container}>
-      <TouchableOpacity onPress={updateDailyContent} style={styles.card}>
+      {/* Päivän vitsi */}
+      <TouchableOpacity onPress={() => {}} style={styles.card}>
         <Text style={styles.title}>Päivän Vitsi:</Text>
         <Text style={styles.content}>{joke}</Text>
       </TouchableOpacity>
-      <TouchableOpacity onPress={updateDailyContent} style={styles.card}>
+
+      {/* Päivän fakta */}
+      <TouchableOpacity onPress={() => {}} style={styles.card}>
         <Text style={styles.title}>Hauska Fakta:</Text>
         <Text style={styles.content}>{funFact}</Text>
+      </TouchableOpacity>
+
+      {/* Suosikkilista */}
+      <ScrollView style={styles.favoriteList}>
+        <Text style={styles.favoriteTitle}>Suosikit:</Text>
+        {favorites.map((item, index) => (
+          <TouchableOpacity key={index} style={styles.favoriteItemContainer} onPress={() => toggleFavorite(item)}>
+            <Text style={styles.favoriteItem}>{item}</Text>
+            <MaterialIcons name="clear" size={24} color="red" />
+          </TouchableOpacity>
+        ))}
+      </ScrollView>
+
+      {/* Tallenna suosikkilista */}
+      <TouchableOpacity onPress={saveFavoritesToFirebase} style={styles.saveFavoritesButton}>
+        <Text style={styles.saveFavoritesButtonText}>Tallenna suosikit</Text>
       </TouchableOpacity>
     </View>
   );
@@ -121,6 +125,39 @@ const styles = StyleSheet.create({
   },
   content: {
     fontSize: 16,
+  },
+  favoriteList: {
+    marginTop: 10,
+    width: '100%',
+    maxHeight: 200,
+  },
+  favoriteTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  favoriteItemContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+  },
+  favoriteItem: {
+    flex: 1,
+    fontSize: 16,
+    marginRight: 10,
+  },
+  saveFavoritesButton: {
+    backgroundColor: 'lightblue',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 10,
+    marginTop: 20,
+  },
+  saveFavoritesButtonText: {
+    fontSize: 16,
+    fontWeight: 'bold',
   },
 });
 
