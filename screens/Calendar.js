8ef866908304a64e95a2697169efd8f8fb0ea206
Roope <roope.nahkala@gmail.com>
2024-04-08 14:27:27 +0300

    päivitystä

--- screens/Calendar.js
+++ screens/Calendar.js
@@ -32,10 +32,14 @@ LocaleConfig.defaultLocale = 'fi';
 const NoteContainer = ({ note, onOpen }) => {
   return (
     <TouchableOpacity onPress={onOpen} style={styles.noteContainer}>
-      <Text style={styles.noteText}>{note}</Text>
+      <Text style={styles.noteText}>
+        <Text style={{ color: '#5A906D' }}>{note.time}  </Text>
+        <Text>{note.text}</Text>
+      </Text>
     </TouchableOpacity>
   );
 };
+
 
 export default function Calendar() {
   const [selectedDate, setSelectedDate] = useState('');
@@ -44,6 +48,8 @@ export default function Calendar() {
   const [modalVisible, setModalVisible] = useState(false);
   const [activeNote, setActiveNote] = useState({ date: '', index: -1, text: '' });
   const [detailsModalVisible, setDetailsModalVisible] = useState(false);
+  const [timeInput, setTimeInput] = useState('');
+
 
   useEffect(() => {
     const loadNotes = async () => {
@@ -65,7 +71,11 @@ export default function Calendar() {
   };
 
   const saveNote = async () => {
-    const updatedNotes = { ...notes, [selectedDate]: [...(notes[selectedDate] || []), noteText] };
+    const newNote = { text: noteText, time: timeInput }; // Use object with text and time
+    const updatedNotes = {
+      ...notes,
+      [selectedDate]: [...(notes[selectedDate] || []), newNote] // Save the newNote object
+    };
     try {
       setNotes(updatedNotes);
       await AsyncStorage.setItem('notes', JSON.stringify(updatedNotes));
@@ -97,15 +107,62 @@ export default function Calendar() {
     }, {});
 
     if (selectedDate) {
-      marked[selectedDate] = { ...marked[selectedDate], selected: true, selectedColor: '#1a8f3f' };
+      marked[selectedDate] = { ...marked[selectedDate], selected: true, selectedColor: '#5A906D' };
     }
     return marked;
   };
 
   const openNoteDetails = (date, index, note) => {
-    setActiveNote({ date, index, text: note });
+    setActiveNote({
+      date,
+      index,
+      text: note.text,
+      time: note.time // Ensure the time is set here
+    });
     setDetailsModalVisible(true);
   };
+
+  const updateNote = async () => {
+    // Copy the notes object
+    const updatedNotes = { ...notes };
+    // Update the specific note with the new text and time
+    if (activeNote.date in updatedNotes && activeNote.index !== -1) {
+      updatedNotes[activeNote.date][activeNote.index] = {
+        text: activeNote.text,
+        time: activeNote.time,
+      };
+      try {
+        await AsyncStorage.setItem('notes', JSON.stringify(updatedNotes));
+        setNotes(updatedNotes); // Update state
+        setDetailsModalVisible(false); // Close the modal
+      } catch (error) {
+        console.error("Failed to update the note in AsyncStorage", error);
+      }
+    }
+  };
+
+  const formatTimeInput = (text) => {
+    const newText = text.replace(/[^0-9]/g, ''); // Remove non-numeric characters
+    let formattedTime = '';
+
+    // Format string as "HH:MM"
+    if (newText.length <= 2) {
+      formattedTime = newText;
+    } else if (newText.length <= 4) {
+      formattedTime = `${newText.slice(0, 2)}:${newText.slice(2)}`;
+    }
+
+    return formattedTime;
+  };
+
+  // Function to convert time "HH:MM" to minutes since midnight
+  const timeToMinutes = (time) => {
+    const [hours, minutes] = time.split(':').map(Number);
+    return hours * 60 + minutes;
+  };
+
+
+
 
   return (
     <View style={{ flex: 1 }}>
@@ -129,26 +186,30 @@ export default function Calendar() {
             />
             {selectedDate && Array.isArray(notes[selectedDate]) && (
               <View style={styles.notesSection}>
-                {notes[selectedDate].map((note, index) => (
-                  <NoteContainer 
-                    key={index} 
-                    note={note}
-                    onOpen={() => openNoteDetails(selectedDate, index, note)} 
-                  />
-                ))}
+                {notes[selectedDate]
+                  .slice() // Create a shallow copy of the array to avoid mutating the original array
+                  .sort((a, b) => timeToMinutes(a.time) - timeToMinutes(b.time)) // Sort notes by time
+                  .map((note, index) => (
+                    <NoteContainer
+                      key={index}
+                      note={note}
+                      onOpen={() => openNoteDetails(selectedDate, index, note)}
+                    />
+                  ))}
               </View>
             )}
+
           </View>
         </TouchableWithoutFeedback>
       </KeyboardAwareScrollView>
-  
+
       <TouchableOpacity
         style={styles.addButton}
         onPress={() => setModalVisible(true)}
       >
         <Icon name="add" size={24} color="#FBFADA" />
       </TouchableOpacity>
-  
+
       <Modal
         animationType="slide"
         transparent={true}
@@ -157,107 +218,184 @@ export default function Calendar() {
       >
         <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
           <View style={styles.modalContent}>
+          <TouchableOpacity 
+        onPress={() => setModalVisible(false)}
+        style={styles.closeButtonStyle}
+      >
+  <Text style={styles.closeButtonText}>Kumoa</Text>
+      </TouchableOpacity>
             <TextInput
               style={styles.noteInput}
-              placeholder="Lisää muistiinpano..."
-              placeholderTextColor="#DDDCDC"
+              placeholder="Lisää tapahtuma..."
+              placeholderTextColor="#9C9C9C"
               value={noteText}
               onChangeText={setNoteText}
               multiline
               numberOfLines={4}
             />
-            <Button title="Tallenna muistiinpano" onPress={saveNote} color="#1a8f3f" />
-            <Button title="Sulje" onPress={() => setModalVisible(false)} color="#1a8f3f" />
-          </View>
-        </TouchableWithoutFeedback>
-      </Modal>
-  
-      <Modal
-        animationType="slide"
-        transparent={true}
-        visible={detailsModalVisible}
-        onRequestClose={() => setDetailsModalVisible(false)}
-      >
-        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-          <View style={styles.modalContent}>
             <TextInput
-              style={styles.noteInput}
-              value={activeNote.text}
-              onChangeText={(text) => setActiveNote(prev => ({ ...prev, text }))}
-              multiline
-              numberOfLines={4}
+              style={styles.timeInput}
+              placeholder="Kellonaika (HH:MM)"
+              placeholderTextColor="#9C9C9C"
+              keyboardType="numeric"
+              maxLength={5}
+              value={timeInput}
+              onChangeText={(text) => setTimeInput(formatTimeInput(text))}
             />
-            <Button title="Save Changes" onPress={() => saveNote()} color="#1a8f3f" />
-            <Button title="Delete Note" onPress={() => deleteNote(activeNote.date, activeNote.index)} color="#ff6347" />
-            <Button title="Close" onPress={() => setDetailsModalVisible(false)} color="#a1a1a1" />
+            <Button title="Tallenna" onPress={saveNote} color="#1a8f3f" />
           </View>
         </TouchableWithoutFeedback>
       </Modal>
+
+      <Modal
+  animationType="slide"
+  transparent={true}
+  visible={detailsModalVisible}
+  onRequestClose={() => setDetailsModalVisible(false)}
+>
+  <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+    <View style={styles.modalContent}>
+      <TouchableOpacity 
+        onPress={() => setDetailsModalVisible(false)} 
+        style={styles.closeButtonStyle}
+      >
+        <Text style={styles.closeButtonText}>Kumoa</Text>
+      </TouchableOpacity>
+
+      {/* The rest of your modal content */}
+      <TextInput
+        style={styles.noteInput}
+        value={activeNote.text}
+        onChangeText={(text) => setActiveNote(prev => ({ ...prev, text }))}
+        multiline
+        numberOfLines={4}
+      />
+      <TextInput
+        style={styles.timeInput}
+        placeholder="Kellonaika (HH:MM)"
+        placeholderTextColor="#9C9C9C"
+        keyboardType="numeric"
+        maxLength={5}
+        value={activeNote.time}
+        onChangeText={(text) => setActiveNote(prevState => ({
+          ...prevState,
+          time: formatTimeInput(text)
+        }))}
+      />
+
+      <Button title="Tallenna" onPress={updateNote} color="#1a8f3f" />
+      <Button title="Poista" onPress={() => deleteNote(activeNote.date, activeNote.index)} color="#ff6347" />
+    </View>
+  </TouchableWithoutFeedback>
+</Modal>
+
     </View>
   );
-                }  
+}
 
-                const styles = StyleSheet.create({
-                  modalContent: {
-                    flex: 1,
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    marginTop: '30%',
-                    marginBottom: '30%',
-                    marginHorizontal: 20,
-                    backgroundColor: '#FBFADA', // Very Light Yellow for modal background
-                    borderRadius: 20,
-                    padding: 35,
-                    shadowColor: '#12372A', // Deep Green for shadow
-                    shadowOffset: {
-                      width: 0,
-                      height: 2,
-                    },
-                    shadowOpacity: 0.25,
-                    shadowRadius: 3.84,
-                    elevation: 5,
-                  },
-                  addButton: {
-                    position: 'absolute',
-                    right: 30,
-                    bottom: 30,
-                    width: 56,
-                    height: 56,
-                    borderRadius: 28,
-                    backgroundColor: '#436850', // Medium Green for add button
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    elevation: 8,
-                    shadowColor: '#000',
-                    shadowOpacity: 0.3,
-                    shadowOffset: { width: 0, height: 2 },
-                    shadowRadius: 4,
-                  },
-                  noteInput: {
-                    width: '100%',
-                    marginBottom: 20,
-                    paddingHorizontal: 10,
-                    paddingVertical: 10,
-                    borderColor: '#436850', // Medium Green for border
-                    borderWidth: 1,
-                    borderRadius: 5,
-                    backgroundColor: '#FBFADA', // Very Light Yellow for input background
-                  },
-                  notesSection: {
-                    padding: 10,
-                    marginTop: 10,
-                  },
-                  noteContainer: {
-                    flexDirection: 'row',
-                    justifyContent: 'space-between',
-                    alignItems: 'center',
-                    marginBottom: 5,
-                    padding: 10,
-                    borderRadius: 5,
-                    backgroundColor: '#ADBC9F', // Light Greenish-Gray for note container
-                  },
-                  noteText: {
-                    flex: 1,
-                    fontSize: 16,
-                  },
-                });
+const styles = StyleSheet.create({
+  modalContent: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: '30%',
+    marginBottom: '30%',
+    marginHorizontal: 20,
+    backgroundColor: '#FFFFFF',
+    borderRadius: 20,
+    padding: 35,
+    shadowColor: '#12372A',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  modalHeader: {
+    position: 'absolute', // Position absolutely to place it on top left
+    top: 0, // Align to the top of the modal
+    left: 0, // Align to the left of the modal
+    width: '100%', // Ensure it spans the width of the modal for alignment
+    padding: 10, // Add some padding around the content
+    alignItems: 'flex-start', // Align items (buttons, etc.) to the start (left)
+  },
+  addButton: {
+    position: 'absolute',
+    right: 30,
+    bottom: 30,
+    width: 56,
+    height: 56,
+    borderRadius: 28,
+    backgroundColor: '#436850', // Medium Green for add button
+    justifyContent: 'center',
+    alignItems: 'center',
+    elevation: 8,
+    shadowColor: '#000',
+    shadowOpacity: 0.3,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 4,
+  },
+  noteInput: {
+    width: '100%',
+    marginBottom: 20,
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    borderColor: '#436850', // Medium Green for border
+    borderWidth: 1,
+    borderRadius: 5,
+    backgroundColor: '#FFFFFF',
+    fontSize: 20, // Slightly larger font
+
+  },
+  notesSection: {
+    padding: 10,
+    marginTop: 10,
+  },
+  noteContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 5,
+    padding: 10,
+    borderRadius: 5,
+    backgroundColor: '#FFF', // Light Greenish-Gray for note container
+    borderWidth: 1, // Sets the thickness of the border
+    borderColor: '#12372A', // Sets the color of the border, choose a color that fits your design
+    borderStyle: 'solid', // Optional, specifies the style of the border, defaults to 'solid'
+  },
+  noteText: {
+    flex: 1,
+    fontSize: 16,
+  },
+  timeInput: {
+    marginBottom: 20,
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    borderColor: '#436850', // Using the same green as for the note input
+    borderWidth: 1,
+    borderRadius: 5,
+    backgroundColor: '#FFFFFF', // Matching the note input background
+    textAlign: 'left', // Align text to the left to match the note input (or 'center' if you prefer)
+    fontSize: 18, // Slightly larger font
+
+  },
+
+  closeButtonStyle: {
+    position: 'absolute', // Position the button absolutely
+    top: 10, // Distance from the top of the modal
+    left: 10, // Distance from the left side of the modal
+    backgroundColor: 'transparent', // Or any background color
+    padding: 10, // Padding for touch area
+    
+  },
+
+  closeButtonText: {
+    color: '#ff6347', // Example text color
+    fontSize: 16, // Adjust font size as needed
+  },
+  
+
+
+});
